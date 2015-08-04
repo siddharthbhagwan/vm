daa498d0c815dcef28046d5dae8fd5709e68b968
Siddhartha Bhagwan <sid_sss@hotmail.com>
2015-08-04 12:02:59 +0530

    photos detection automated

--- js/application.js
+++ js/application.js
@@ -1,7 +1,17 @@
 $(document).ready(function(){
 
-	$("#lightGallery").lightGallery({
-		 
+	$("#read-more").click(function(){
+  	if($("#read-more-points").is(":visible")){
+  		$("#read-more").html("<u>Read more...</u>");
+  		$("#read-more-points").hide();
+  	} else {
+  		$("#read-more").html("<u>Read less...</u>");
+  		$("#read-more-points").show();
+  	}
+	});
+
+  $("#lightGallery").lightGallery({
+
     mode      : 'slide',  // Type of transition between images. Either 'slide' or 'fade'.
     useCSS    : true,     // Whether to always use jQuery animation for transitions or as a fallback.
     cssEasing : 'ease',   // Value for CSS "transition-timing-function".
@@ -23,14 +33,14 @@ $(document).ready(function(){
     thumbMargin          : 5,        // Spacing between each thumbnails 
 
     controls         : true,  // Whether to display prev/next buttons.
-    hideControlOnEnd : false, // If true, prev/next button will be hidden on first/last image.
+    hideControlOnEnd : true, // If true, prev/next button will be hidden on first/last image.
     loop             : false, // Allows to go to the other end of the gallery at first/last img.
     auto             : false, // Enables slideshow mode.
     pause            : 4000,  // Delay (in ms) between transitions in slideshow mode.
     escKey           : true,  // Whether lightGallery should be closed when user presses "Esc".
     closable         : true,  //allows clicks on dimmer to close gallery
 
-    counter      : false, // Shows total number of images and index number of current image.
+    counter      : true, // Shows total number of images and index number of current image.
     lang         : { allPhotos: 'All photos' }, // Text of labels.
 
     mobileSrc         : false, // If "data-responsive-src" attr. should be used for mobiles.
@@ -38,25 +48,6 @@ $(document).ready(function(){
     swipeThreshold    : 50,    // How far user must swipe for the next/prev image (in px).
     enableTouch       : true,  // Enables touch support
     enableDrag        : true,  // Enables desktop mouse drag support
+  });
 
-    // Callbacks el = current plugin object
-    onOpen        : function(el) {}, // Executes immediately after the gallery is loaded.
-    onSlideBefore : function(el) {}, // Executes immediately before each transition.
-    onSlideAfter  : function(el) {}, // Executes immediately after each transition.
-    onSlideNext   : function(el) {}, // Executes immediately before each "Next" transition.
-    onSlidePrev   : function(el) {}, // Executes immediately before each "Prev" transition.
-    onBeforeClose : function(el) {}, // Executes immediately before the start of the close process.
-    onCloseAfter  : function(el) {}, // Executes immediately once lightGallery is closed.
-
-   });
-
-	$("#read-more").click(function(){
-		if($("#read-more-points").is(":visible")){
-			$("#read-more").html("<u>Read more...</u>");
-			$("#read-more-points").hide();
-		} else {
-			$("#read-more").html("<u>Read less...</u>");
-			$("#read-more-points").show();
-		}
-	})
 });
